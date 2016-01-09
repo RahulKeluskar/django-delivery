076c168bd7fc496bc421868cfa90f373fa925336
geezhawk <jonathan@jcoxjourno.com>
2016-01-10 00:16:41 +0700

    added default mixins to restaurants viewset

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
 
-from rest_framework import viewsets
+from rest_framework import authentication, permissions, viewsets
 
 from .models import Restaurant
 from .serializers import RestaurantSerializer
 
-class RestaurantViewSet(viewsets.ModelViewSet):
+class RestaurantViewSet(DefaultsMixin, viewsets.ModelViewSet):
     """API endpoint for listing and creating restaurants"""
 
     queryset = Restaurant.objects.order_by('name')
     serializer_class = RestaurantSerializer
+
+class DefaultsMixin(object):
+    """Default setings for view authentication, pagination, 
+    permissions, and filtering."""
+
+    authentication_classes = (
+        authentication.BasicAuthentication,
+        authentication.TokenAuthentication
+    )
